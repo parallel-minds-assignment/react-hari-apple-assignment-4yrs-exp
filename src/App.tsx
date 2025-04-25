05500483ea43be75f12e3891856f5a89ce13ace2
parallel-minds <kislay.kislay@parallel-minds.com>
2025-04-25 21:06:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SearchInput } from './features/search/SearchInput.js';
 const App = () => {
   return (
     <div className="App">
-      <h1 className='apple-gradient-text'>🎬 Instant Movie Search</h1>
+      <h1 className='apple-gradient-text'>Search Movies</h1>
       <SearchInput />
     </div>
   );
